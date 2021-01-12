e3a2f014a0441bee289ced17f02832c704324bb2
NVC <nunovicentecruz@outlook.com>
2021-01-12 12:25:54 +0000

    Added user input to personalize with user's name.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("What's your name?");
+        Scanner keyboard = new Scanner(System.in);
+        String name=keyboard.nextLine();
+        System.out.println("Welcome "+name+"!");
     }
 }
